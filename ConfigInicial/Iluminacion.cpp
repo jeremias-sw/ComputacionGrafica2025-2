9b1f8f4cb18dad7b0f54a11da155aeaad07faf4f
jeremias-sw <jeremiassw97@gmail.com>
2025-03-30 20:08:49 -0600

    Se añade la práctica 8

--- ConfigInicial/Iluminacion.cpp
+++ ConfigInicial/Iluminacion.cpp
@@ -1,8 +1,8 @@
 /*
 Sánchez Wilhelm Jeremias
 314309064
-Previo 8
-Fecha de entrega: 25 de marzo de 2025
+Práctica 8
+Fecha de entrega: 30 de marzo de 2025
 */
 
 
@@ -46,12 +46,14 @@ bool firstMouse = true;
 
 
 // Light attributes
-glm::vec3 lightPos(0.5f, 0.5f, 2.5f);   // posición de la fuente de luz
+glm::vec3 lightPos(22.5f, 16.5f, 2.5f);   // posición de la fuente de luz
 float movelightPos = 0.0f;              // variable para manipular el movimiento de la fuente
 GLfloat deltaTime = 0.0f;               // elementos para el intercambio
 GLfloat lastFrame = 0.0f;               // después de cada frame
 float rot = 0.0f;                       // variable para rotación
-bool activanim = false;                 
+bool activanim = false;     
+
+bool esLuna = false;  // variable para el cambio de iluminación
 
 int main()
 {
@@ -111,6 +113,8 @@ int main()
     // Load models
     Model red_dog((char*)"Models/RedDog.obj");
     Model orange((char*)"Models/orange/source/fr_caraOrange/fr_caraOrange.obj");
+    Model casa((char*)"Models/casa/lowpoly_land2.obj");
+    Model moon((char*)"Models/moon/textures/moon_2.obj");
     glm::mat4 projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
 
     float vertices[] = {
@@ -224,12 +228,19 @@ int main()
         glUniform3f(lightPosLoc, lightPos.x + movelightPos, lightPos.y + movelightPos, lightPos.z + movelightPos); // se define la posición 
         glUniform3f(viewPosLoc, camera.GetPosition().x, camera.GetPosition().y, camera.GetPosition().z); // matriz de la cámara
 
-
-        // Set lights properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.9f, 0.9f, 0.9f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 0.7f, 0.7f, 0.7f); 
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 0.9f, 0.9f, 0.9f);
-
+        // Con el booleano establecido previamente se definen los parámetros de la iluminación a usarse
+        if (esLuna) {
+            // Set lights properties (luz nocturna)
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.5f, 0.5f, 0.5f); 
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 0.4f, 0.4f, 0.8f);   // luz con altos niveles de azul
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 0.4f, 0.4f, 0.4f);
+        }
+        else {
+            // Set lights properties (luz diurna)
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 807.3f, 807.3f, 807.3f);
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.diffuse"), 0.9f, 0.8f, 0.6f);       // luz cálida
+            glUniform3f(glGetUniformLocation(lightingShader.Program, "light.specular"), 0.9f, 0.9f, 0.9f);
+        }
 
 
         glUniform3f(glGetUniformLocation(lightingShader.Program, "light.ambient"), 0.0f, 0.0f, 0.0f);
@@ -238,44 +249,58 @@ int main()
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
 
         // Set material properties
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.ambient"), 0.7f, 0.6f, 0.6f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.diffuse"), 0.8f, 0.8f, 0.8f);
-        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.specular"), 1.0f, 1.0f, 1.0f);
-        glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 0.3f);
-
-
-
-
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.ambient"), 0.4f, 0.4f, 0.4f);    // propiedades
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.diffuse"), 0.8f, 0.8f, 0.8f);    // relativamente
+        glUniform3f(glGetUniformLocation(lightingShader.Program, "material.specular"), 0.3f, 0.3f, 0.3f);   // neutrales
+        glUniform1f(glGetUniformLocation(lightingShader.Program, "material.shininess"), 0.1f);              // para los objetos
 
 
         // Draw the loaded model
         glm::mat4 model(1);
+        model = glm::translate(model, glm::vec3(10.0f, 0.0f, 0.0f));
+        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+        glBindVertexArray(VAO);
+        casa.Draw(lightingShader);         // se dibuja el escenario (casa con río)
+
+        model = glm::translate(model, glm::vec3(3.0f, 4.75f, 0.0f));
         model = glm::scale(model, glm::vec3(3.0f, 3.0f, 3.0f));
         glUniformMatrix4fv(glGetUniformLocation(lightingShader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
         glBindVertexArray(VAO);
-        red_dog.Draw(lightingShader);
+        red_dog.Draw(lightingShader);       // se dibuja el perrito en el escenario
 
         //glDrawArrays(GL_TRIANGLES, 0, 36);
         
-        model = glm::translate(model, glm::vec3(2.0f, 3.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-        red_dog.Draw(lightingShader);
+       
 
         glBindVertexArray(0);
-
-
 
 
         lampshader.Use();
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
         glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos + movelightPos);
-        model = glm::scale(model, glm::vec3(0.3f));
-        glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        glBindVertexArray(0);
+
+        if (esLuna) {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, lightPos + movelightPos);
+            model = glm::scale(model, glm::vec3(0.5f)); // escala pequeña
+            glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+            glBindVertexArray(VAO);
+            moon.Draw(lampshader);              // se dibuja el modelo de la luna (sin texturas)
+            glBindVertexArray(0);
+        }
+        else {
+            model = glm::mat4(1.0f);
+            model = glm::translate(model, lightPos + movelightPos);
+            model = glm::scale(model, glm::vec3(1.0f)); // mayor escala en representación del sol
+            glUniformMatrix4fv(glGetUniformLocation(lampshader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
+            glBindVertexArray(VAO);
+            moon.Draw(lampshader);  // se dibuja el sol (sin texturas)
+            glBindVertexArray(0);
+        }
+
+        
+        
 
         // Swap the buffers
         glfwSwapBuffers(window);
@@ -319,6 +344,23 @@ void DoMovement()
             rot -= 0.1f;
     }
 
+    if (keys[GLFW_KEY_O]) // Mover el sol en su órbita
+    {
+        rot += 0.002f; // Incrementa el ángulo (ajustable para velocidad)
+        if (rot > glm::pi<float>()) // Limita a media circunferencia
+            rot = glm::pi<float>();
+    }
+    if (keys[GLFW_KEY_L]) // Retroceder el sol
+    {
+        rot -= 0.002f;
+        if (rot < 0.0f)
+            rot = 0.0f;
+    }
+
+    // Movimiento semicircular con radio 8.0f
+    float radius = 21.0f;
+    lightPos.x = radius * cos(rot) + 3.5f;
+    lightPos.y = radius * sin(rot) + 3.5f; // Elevación inicial en y
 }
 
 // Is called whenever a key is pressed/released via GLFW
@@ -341,7 +383,7 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
         }
     }
 
-    if (keys[GLFW_KEY_O])
+    /*if (keys[GLFW_KEY_O])
     {
        
         movelightPos += 0.1f;
@@ -351,6 +393,11 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
     {
         
         movelightPos -= 0.1f;
+    }*/
+
+    if (key == GLFW_KEY_N && action == GLFW_PRESS) {
+
+        esLuna = !esLuna;
     }
 
 
